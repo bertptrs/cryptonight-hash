2653c56a8b80312034e066383fb5522ac0893867
Bert Peters <bert@bertptrs.nl>
2019-10-15 00:07:59 +0200

    Clippy fixes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use digest::{Digest, FixedOutput, Input, Reset};
-use digest::generic_array::GenericArray;
 use digest::generic_array::typenum::U32;
+use digest::generic_array::GenericArray;
+use digest::{FixedOutput, Input, Reset};
 
 use aes::aes_round;
 use aes::derive_key;
@@ -37,8 +37,7 @@ impl FixedOutput for CryptoNight {
         let blocks = &mut keccac[64..192];
 
         let mut scratch_pad = Vec::<u8>::with_capacity(SCRATCH_PAD_SIZE);
-        // TODO: don't actually initialize the data.
-        scratch_pad.resize(SCRATCH_PAD_SIZE, 0);
+        unsafe { scratch_pad.set_len(SCRATCH_PAD_SIZE) };
 
         for scratchpad_chunk in scratch_pad.chunks_exact_mut(blocks.len()) {
             for block in blocks.chunks_exact_mut(16) {
@@ -56,13 +55,21 @@ impl FixedOutput for CryptoNight {
 
 #[cfg(test)]
 mod tests {
+    use digest::Digest;
+
     use super::*;
 
     #[test]
     fn validate_samples() {
         // Samples taken from
-        validate_sample(b"", b"eb14e8a833fac6fe9a43b57b336789c46ffe93f2868452240720607b14387e11");
-        validate_sample(b"This is a test", b"a084f01d1437a09c6985401b60d43554ae105802c5f5d8a9b3253649c0be6605");
+        validate_sample(
+            b"",
+            b"eb14e8a833fac6fe9a43b57b336789c46ffe93f2868452240720607b14387e11",
+        );
+        validate_sample(
+            b"This is a test",
+            b"a084f01d1437a09c6985401b60d43554ae105802c5f5d8a9b3253649c0be6605",
+        );
     }
 
     fn validate_sample(input: &[u8], hash: &[u8]) {
